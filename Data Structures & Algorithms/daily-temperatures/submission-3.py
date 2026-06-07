4724cd1062104652675d985b93b60e39efda6e80
Ethan Tran <tran.ethan@hotmail.com>
2026-06-07 10:37:00 -0400

    Add: daily-temperatures - submission-3

---
+++ Data Structures & Algorithms/daily-temperatures/submission-3.py
@@ -0,0 +1,32 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        result = [0] * len(temperatures)
+
+        stack = [] # (i, temperature)
+
+        # for i in range(len(temperatures) - 1, -1, -1):
+        #     temp = temperatures[i]
+        #     # keep popping until no elements or next element is greater
+        #     while stack and stack[-1][1] <= temp:
+        #         stack.pop()
+            
+        #     if stack:
+        #         # there is element greater than current temp
+        #         diff = stack[-1][0] - i
+        #         result[i] = diff
+        #     else:
+        #         # no greater element than current temp
+        #         result[i] = 0
+
+        #     stack.append((i, temp))
+
+        # return result
+
+        for index, temp in enumerate(temperatures):
+            while stack and stack[-1][1] < temp:
+                i, tmp = stack.pop()
+                result[i] = index - i
+            stack.append((index, temp))
+        
+        return result
+            
