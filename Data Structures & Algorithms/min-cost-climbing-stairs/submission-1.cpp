db10d39622eaf2c113a515889d12a0ad08c83007
Ethan Tran <tran.ethan@hotmail.com>
2026-06-18 23:30:23 -0400

    Add: min-cost-climbing-stairs - submission-1

---
+++ Data Structures & Algorithms/min-cost-climbing-stairs/submission-1.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minCostClimbingStairs(vector<int>& cost) {
+        std::vector<int> dp(cost.size());
+
+        for (int i = 0; i < 2; i++) {
+            dp[i] = cost[i];
+        }
+
+        for (int i = 2; i < dp.size(); i++) {
+            dp[i] = std::min(dp[i-1], dp[i - 2]) + cost[i];
+        }
+
+        return std::min(dp[dp.size() - 1], dp[dp.size() - 2]);
+    }
+};
